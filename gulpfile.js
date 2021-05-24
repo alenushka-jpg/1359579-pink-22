a6d023398b18204b8d550441dca2646e43c2221b
Alyona <alionxlo@gmail.com>
2021-05-24 18:31:01 +0300

    module5-task2

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,9 @@ const rename = require("gulp-rename");
 const htmlmin = require("gulp-htmlmin");
 const terser = require("gulp-terser");
 const imagemin = require("gulp-imagemin");
-// const webp = require("gulp-webp");
-// const svgstore = require("gulp-svgstore");
-// const del = require("del");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
 const sync = require("browser-sync").create();
 
 // Styless
@@ -22,7 +22,10 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer(),
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(postcss([
       csso()
     ]))
     .pipe(rename("style.min.css"))
@@ -67,14 +70,14 @@ const optimizeImages = () => {
     .pipe(gulp.dest("build/img"))
 }
 
-exports.images = optimizeImages;
+exports.optimizeImages = optimizeImages;
 
 const copyImages = () => {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(gulp.dest("build/img"))
 }
 
-exports.images = copyImages;
+exports.copyImages = copyImages;
 
 // WebP
 
@@ -127,7 +130,7 @@ const clean = () => {
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -148,7 +151,7 @@ const reload = (done) => {
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
   gulp.watch("source/js/script.js", gulp.series(scripts));
   gulp.watch("source/*.html", gulp.series(html, reload));
 }
@@ -163,7 +166,7 @@ const build = gulp.series(
     scripts,
     sprite,
     createWebp
-  ),
+  )
 );
 
 exports.build = build;
@@ -184,4 +187,5 @@ exports.default = gulp.series(
   gulp.series(
     server,
     watcher
-  ));
+  )
+);
